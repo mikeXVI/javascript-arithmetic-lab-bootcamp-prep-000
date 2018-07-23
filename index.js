7f2b04b36a187543bd32a2fd859ea1acd01921c3
Michael Leahy <mleahy28@gmail.com>
2018-07-23 22:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function add(arg1, arg2){
   return arg1 + arg2
 }
 
-function add(arg1, arg2){
-  return arg1 + arg2
+function subtract(arg1, arg2){
+  return arg1 - arg2
 }
 
-function add(arg1, arg2){
-  return arg1 + arg2
+function multiply(arg1, arg2){
+  return arg1 * arg2
 }
 
-function add(arg1, arg2){
-  return arg1 + arg2
+function divide(arg1, arg2){
+  return arg1 / arg2
 }
