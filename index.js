7f520b94183c07ce43197406298c7c1a5185d0c0
Michael Leahy <mleahy28@gmail.com>
2018-07-23 22:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function multiply(arg1, arg2){
 function divide(arg1, arg2){
   return arg1 / arg2
 }
+
+function inc(arg){
+  return arg++
+}
+
+function dec(arg){
+  return arg--
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
